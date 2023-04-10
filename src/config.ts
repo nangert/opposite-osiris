40fac3ff80e03ec4150e2cd7fdb7268235151e38
nangert <angert.niklas@gmail.com>
2023-04-10 18:41:29 +0200

    some minor updates

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://duelystranks.deno.dev/",
+  author: "Nangert",
+  desc: "Live rankings for Duelyst Ladder and Gauntlet.",
+  title: "DuelystRanks",
+  ogImage: "alcuincarousel.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
