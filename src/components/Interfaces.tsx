7c3c5088342f02856ea9b2dd137823d700d06b22
nangert <angert.niklas@gmail.com>
2023-04-15 01:28:50 +0200

    working history

---
+++ src/components/Interfaces.tsx
@@ -0,0 +1,54 @@
+export interface PlayerData {
+    username: string;
+    rating: number;
+    place: number;
+}
+
+export interface Player {
+    username: string;
+    user_id: string;
+}
+
+export interface MatchHistoryItem {
+    user_id: string;
+    game_id: string;
+    game_type: string;
+    game_server: string;
+    gauntlet_ticket_id: number | null;
+    is_scored: boolean;
+    is_winner: boolean;
+    is_draw: boolean;
+    is_player_1: boolean;
+    faction_id: number;
+    general_id: number;
+    faction_xp: number;
+    faction_xp_earned: number;
+    opponent_id: string;
+    opponent_faction_id: number;
+    opponent_general_id: number;
+    opponent_username: string;
+    game_version: string;
+    rewards: string | null;
+    reward_ids: string | null;
+    rank_before: number;
+    rank_stars_before: number;
+    rank_delta: number;
+    rank_stars_delta: number;
+    rank_win_streak: number;
+    is_daily_win: boolean;
+    play_count_reward_progress: number;
+    win_count_reward_progress: number;
+    has_maxed_play_count_rewards: boolean;
+    has_maxed_win_count_rewards: boolean;
+    created_at: number;
+    updated_at: number;
+    ended_at: number;
+    status: string;
+    gold_tip_amount: number | null;
+    rift_ticket_id: null;
+    rift_points: null;
+    rift_points_earned: null;
+    rift_rating_after: null;
+    rift_rating_earned: null;
+    digest: string;
+}
--- src/components/MatchHistory.tsx
+++ src/components/MatchHistory.tsx
@@ -1,6 +1,7 @@
 import Fuse from "fuse.js";
 import { useState, useEffect } from "react";
 import "./MatchHistory.css";
+import type { MatchHistoryItem } from "./Interfaces";
 
 // Configs fuse.js
 // https://fusejs.io/api/options.html
@@ -10,60 +11,12 @@ const fuseOptions = {
     threshold: 0, // adjust the threshold based on your needs
 };
 
-interface PlayerData {
-    username: string;
-    rating: number;
-    place: number;
-}
+const databaseUrl = import.meta.env.SECRET_DATABASEKEY;
+const matchHistoryStart = import.meta.env.PUBLIC_MATCH_HISTORY_START;
+const matchHistoryEnd = import.meta.env.PUBLIC_MATCH_HISTORY_END;
+const token = import.meta.env.PUBLIC_DUELYST_TOKEN;
 
-interface Player {
-    username: string;
-    user_id: string;
-}
-
-interface MatchHistoryItem {
-    user_id: string;
-    game_id: string;
-    game_type: string;
-    game_server: string;
-    gauntlet_ticket_id: number | null;
-    is_scored: boolean;
-    is_winner: boolean;
-    is_draw: boolean;
-    is_player_1: boolean;
-    faction_id: number;
-    general_id: number;
-    faction_xp: number;
-    faction_xp_earned: number;
-    opponent_id: string;
-    opponent_faction_id: number;
-    opponent_general_id: number;
-    opponent_username: string;
-    game_version: string;
-    rewards: string | null;
-    reward_ids: string | null;
-    rank_before: number;
-    rank_stars_before: number;
-    rank_delta: number;
-    rank_stars_delta: number;
-    rank_win_streak: number;
-    is_daily_win: boolean;
-    play_count_reward_progress: number;
-    win_count_reward_progress: number;
-    has_maxed_play_count_rewards: boolean;
-    has_maxed_win_count_rewards: boolean;
-    created_at: number;
-    updated_at: number;
-    ended_at: number;
-    status: string;
-    gold_tip_amount: number | null;
-    rift_ticket_id: null;
-    rift_points: null;
-    rift_points_earned: null;
-    rift_rating_after: null;
-    rift_rating_earned: null;
-    digest: string;
-}
+const faction_ids = ["Lyonar", "Songhai", "Vetruvian", "Abyssian", "Magmar", "Vanar"];
 
 // const pool = new postgres.Pool(databaseUrl, 3, true);
 
@@ -77,21 +30,15 @@ interface MatchHistoryItem {
 // //     return await connection.queryobject`select username, user_id from players where username = ${username}`;
 // // };
 
-function MatchHistory(username: string) {
-    const faction_ids = ["Lyonar", "Songhai", "Vetruvian", "Abyssian", "Magmar", "Vanar"];
-    const databaseUrl = import.meta.env.SECRET_DATABASEKEY;
-    const matchHistoryStart = import.meta.env.PUBLIC_MATCH_HISTORY_START;
-    const matchHistoryEnd = import.meta.env.PUBLIC_MATCH_HISTORY_END;
-    const token = import.meta.env.SECRET_DUELYST_TOKEN;
-
-    const [matches, setMatches] = useState([]);
+const MatchHistory = (username: string) => {
     const [loading, setLoading] = useState(true);
+    const [query, setQuery] = useState("");
+    const [matches, setMatches] = useState([] as MatchHistoryItem[]);
 
     useEffect(() => {
-        const fetchPlayerHistory = async () => {
-            const url = matchHistoryStart.trim() + "-NJW19A1jvOyXiVn-Wy7" + matchHistoryEnd.trim();
+        const fetchItems = async (user_id: string) => {
             try {
-                const response = await fetch(url, {
+                const response = await fetch(matchHistoryStart + user_id + matchHistoryEnd, {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
@@ -102,19 +49,17 @@ function MatchHistory(username: string) {
                 setMatches(data);
                 setLoading(false);
             } catch (error) {
-                console.error("Error fetching items: ", error);
+                console.error("Error fetching items:", error);
                 setLoading(false);
             }
         };
-
-        fetchPlayerHistory;
-    });
+        const user_id = "-NJW19A1jvOyXiVn-Wy7";
+        fetchItems(user_id);
+    }, [token, matchHistoryStart, matchHistoryEnd]);
 
     if (loading) {
-        return <p>Loading items...</p>;
+        return <p>{token}</p>;
     }
-
-    const [query, setQuery] = useState("");
 
     const fuse = new Fuse(matches, fuseOptions);
 
@@ -126,18 +71,18 @@ function MatchHistory(username: string) {
     }
 
     return (
-        <>
+        <div>
             <label>Filter players:</label>
             <input type="text" value={query} onChange={() => handleOnSearch} placeholder="Search posts" />
-
+            <br />
             <ul id="list">
-                {posts &&
-                    posts.map((match: MatchHistoryItem) => (
+                {matches &&
+                    matches.map((match: MatchHistoryItem) => (
                         <li>
                             {match.is_player_1 && (
                                 <h4>
                                     <label id={match.is_winner ? "winner" : "neutral"}>
-                                        {username} ({faction_ids[match.faction_id - 1]})
+                                        {"username"} ({faction_ids[match.faction_id - 1]})
                                     </label>
                                     -{" "}
                                     <label id={!match.is_winner ? (match.is_draw ? "neutral" : "loser") : "neutral"}>
@@ -153,15 +98,15 @@ function MatchHistory(username: string) {
                                     </label>
                                     -{" "}
                                     <label id={match.is_winner ? "winner" : "neutral"}>
-                                        {username} ({faction_ids[match.faction_id - 1]})
+                                        {"username"} ({faction_ids[match.faction_id - 1]})
                                     </label>
                                 </h4>
                             )}
                         </li>
                     ))}
             </ul>
-        </>
+        </div>
     );
-}
+};
 
 export default MatchHistory;
