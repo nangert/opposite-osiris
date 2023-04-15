b22e9945db4589e63ed5deaf8228c3ce57e90303
nangert <angert.niklas@gmail.com>
2023-04-15 15:02:17 +0200

    filters

--- src/components/MatchHistory/MatchHistory.tsx
+++ src/components/MatchHistory/MatchHistory.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import "./MatchHistory.css";
 import type { Match } from "../Interfaces";
 import MatchHistoryItem from "./MatchHistoryItem";
-import { TextInput } from "@mantine/core";
+import { TextInput, Checkbox, Grid } from "@mantine/core";
 
 interface MatchHistoryProps {
     player: string;
@@ -40,6 +40,12 @@ const MatchHistory: React.FC<MatchHistoryProps> = ({ player }) => {
     const [loading, setLoading] = useState(true);
     const [query, setQuery] = useState("");
     const [matches, setMatches] = useState([] as Match[]);
+
+    const [onlyWins, setOnlyWins] = useState(false);
+    const [onlyLosses, setOnlyLosses] = useState(false);
+
+    const [youFactions, setYouFactions] = useState([] as number[]);
+    const [oppFactions, setOppFactions] = useState([] as number[]);
 
     useEffect(() => {
         const fetchItems = async (user_id: string) => {
@@ -79,27 +85,77 @@ const MatchHistory: React.FC<MatchHistoryProps> = ({ player }) => {
         setQuery(event.target.value);
     };
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        console.log("Submitted value:", query);
+    const toggleYouFaction = (faction: number) => {
+        youFactions.includes(faction) ? setYouFactions(youFactions.filter(x => x !== faction)) : youFactions.push(faction);
+    };
+
+    const toggleOppFaction = (faction: number) => {
+        oppFactions.includes(faction) ? setOppFactions(oppFactions.filter(x => x !== faction)) : oppFactions.push(faction);
+    };
+
+    const filterMatches = (matches: Match[]) => {
+        let filtered = matches;
+        if (onlyWins) {
+            filtered = filtered.filter(x => x.is_winner === true);
+        }
+        if (onlyLosses) {
+            filtered = filtered.filter(x => x.is_winner === false);
+        }
+        if (youFactions.length > 0) {
+            filtered = filtered.filter(x => youFactions.includes(x.faction_id));
+        }
+        if (oppFactions.length > 0) {
+            filtered = filtered.filter(x => oppFactions.includes(x.opponent_faction_id));
+        }
+
+        return filtered;
     };
 
     return (
         <div>
-            <label>Filter players:</label>
-            <TextInput label="Player name" description="Player name" onChange={handleSearch} value={query} />
+            <h3>Filter players:</h3>
+            <Grid>
+                <Grid.Col>
+                    <TextInput label="Player name" description="Player name" onChange={handleSearch} value={query} />
+                </Grid.Col>
+                <Grid.Col span={6}>
+                    <h2>Your faction:</h2>
+                    <Checkbox label="Lyonar" checked={youFactions.includes(1)} onChange={() => toggleYouFaction(1)} />
+                    <Checkbox label="Songhai" checked={youFactions.includes(2)} onChange={() => toggleYouFaction(2)} />
+                    <Checkbox label="Vetruvian" checked={youFactions.includes(3)} onChange={() => toggleYouFaction(3)} />
+                    <Checkbox label="Abyssian" checked={youFactions.includes(4)} onChange={() => toggleYouFaction(4)} />
+                    <Checkbox label="Magmar" checked={youFactions.includes(5)} onChange={() => toggleYouFaction(5)} />
+                    <Checkbox label="Vanar" checked={youFactions.includes(6)} onChange={() => toggleYouFaction(6)} />
+                </Grid.Col>
+                <Grid.Col span={6}>
+                    <h2>Opponents faction:</h2>
+                    <Checkbox label="Lyonar" checked={oppFactions.includes(1)} onChange={() => toggleOppFaction(1)} />
+                    <Checkbox label="Songhai" checked={oppFactions.includes(2)} onChange={() => toggleOppFaction(2)} />
+                    <Checkbox label="Vetruvian" checked={oppFactions.includes(3)} onChange={() => toggleOppFaction(3)} />
+                    <Checkbox label="Abyssian" checked={oppFactions.includes(4)} onChange={() => toggleOppFaction(4)} />
+                    <Checkbox label="Magmar" checked={oppFactions.includes(5)} onChange={() => toggleOppFaction(5)} />
+                    <Checkbox label="Vanar" checked={oppFactions.includes(6)} onChange={() => toggleOppFaction(6)} />
+                </Grid.Col>
+                <Grid.Col>
+                    <Checkbox label="Only your wins" onChange={() => setOnlyWins(!onlyWins)} />
+                </Grid.Col>
+                <Grid.Col>
+                    <Checkbox label="Only opponents wins" onChange={() => setOnlyLosses(!onlyLosses)} />
+                </Grid.Col>
+            </Grid>
+
             <br />
 
             <ul id="list">
                 {posts &&
                     query.length >= 3 &&
-                    posts.map((match: Match) => (
+                    filterMatches(posts).map((match: Match) => (
                         <>
                             <MatchHistoryItem match={match} player={player} />
                         </>
                     ))}
                 {!(posts && query.length >= 3) &&
-                    matches.map((match: Match) => (
+                    filterMatches(matches).map((match: Match) => (
                         <>
                             <MatchHistoryItem match={match} player={player} />
                         </>
