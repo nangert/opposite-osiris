abe5799e98f1bb7cfaceaf4caa6603d91bc98b0b
nangert <angert.niklas@gmail.com>
2023-06-22 13:16:01 +0200

    Update PlayerRoot.tsx

--- src/components/MatchHistory/PlayerRoot.tsx
+++ src/components/MatchHistory/PlayerRoot.tsx
@@ -19,7 +19,7 @@ const matchHistoryStart = "https://api.duelyst2.com/api/users/";
 const matchHistoryEnd = "/games?len=9999&blatmmr=true";
 
 const token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImlkIjoiLU5UNFhVSlF4dnlpcTRsMEx1djMiLCJlbWFpbCI6ImR1ZWx5c3RyYW5rc0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImR1ZWx5c3RyYW5rcyJ9LCJ2IjowLCJpYXQiOjE2ODYxNzY1MDYsImV4cCI6MTY4NzM4NjEwNn0.vyQ86iUCvC4PotiLM-eMoKGV4YiQY4QxoM1mp0eI0Sc";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImlkIjoiLU5UNFhVSlF4dnlpcTRsMEx1djMiLCJlbWFpbCI6ImR1ZWx5c3RyYW5rc0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImR1ZWx5c3RyYW5rcyJ9LCJ2IjowLCJpYXQiOjE2ODcwNzQ2OTYsImV4cCI6MTY4ODI4NDI5Nn0.uezri_TVC4Z_S01Ym3my0UHIYF6ponkmMCaI2sHxJZs";
 
 const PlayerRoot = () => {
     const { username, setUsername, history, setHistory, loading, setLoading, showHistory, setShowHistory } = useContext(PlayerHistoryContext);
